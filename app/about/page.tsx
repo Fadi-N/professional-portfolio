3cb9104d5f1b18fed4c059c82c9d8e3fa3c8252b
Fadi <fadi@Fadis-MacBook-Pro.local>
2025-02-16 17:31:06 +0100

    change | font sizes and paddings adjustment in about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Image from "next/image";
+import {FaGithub, FaLinkedinIn} from "react-icons/fa6";
 
 const AboutPage = () => {
     return (
         <main className="about">
             <div className="about-intro">
-                <div className="flex justify-center items-center rounded-xl relative w-[450px] h-[600px]">
+                <div
+                    className="about-intro-image-container">
                     <Image
                         src="/about-picture.jpg"
                         alt="react.png"
@@ -14,31 +16,30 @@ const AboutPage = () => {
                         className="rounded-xl"
                     />
                 </div>
-                <div className="flex-1 flex flex-col py-6 space-y-10">
-                    <div className="flex-1">
+                <div className="about-intro-content-container">
+                    <div>
                         <h1>Frontend Developer, </h1>
                         <h1>Graduated in Computer Science, </h1>
                         <h1>Building modern web apps.</h1>
                     </div>
-                    <div className="flex-1 flex flex-col space-y-8">
-                        <p>
-                            I'm Fadi, a <span className="font-semibold">Frontend Developer</span> who loves building
-                            smooth, fast, and good-looking web apps.
-                            I am pursuing my Master's degree at the Warsaw University of Technology.
-                        </p>
-                        <p>
-                            I mainly work with React and Next.js, creating scalable UX/UI and making sure everything
-                            runs
-                            fast and looks great.
-                            I enjoy working with different teams, improving web performance, and bringing ideas to life
-                            through clean and efficient code.
-                        </p>
-                    </div>
+
+                    <p>
+                        I'm Fadi, a <span className="font-semibold">Frontend Developer</span> who loves building
+                        smooth, fast, and good-looking web apps.
+                        I am pursuing my Master's degree at the Warsaw University of Technology.
+                    </p>
+                    <p>
+                        I mainly work with React and Next.js, creating scalable UX/UI and making sure everything
+                        runs
+                        fast and looks great.
+                        I enjoy working with different teams, improving web performance, and bringing ideas to life
+                        through clean and efficient code.
+                    </p>
                 </div>
             </div>
             <div className="about-details">
                 <div className="flex-1 flex flex-col space-y-2">
-                    <h6>Skills and Interest</h6>
+                    <h4>Skills and Interest</h4>
                     <div className="flex flex-col space-y-4 text-sm">
                         <div>
                             <p>
@@ -72,53 +73,58 @@ const AboutPage = () => {
                         </div>
                     </div>
                 </div>
-                <div className="flex-1 flex flex-col space-y-2">
-                    <h6>Experience</h6>
-                    <div className="flex flex-col space-y-4 text-sm">
-                        <div>
-                            <p className="font-semibold">Schoenberger Germany Enterprises</p>
-                            <p>03.2022 - </p>
-                            <p>
-                                Frontend Developer • React web apps • UI development • Performance optimization • Global
-                                collaboration on UX
-                            </p>
+                <div className="flex-1 flex flex-col space-y-4 md:space-y-8 lg:space-y-10 xl:space-y-12 2xl:space-y-14">
+                    <div className="flex flex-col space-y-2">
+                        <h4>Experience</h4>
+                        <div className="flex flex-col space-y-4 text-sm">
+                            <div>
+                                <h5>Schoenberger Germany Enterprises</h5>
+                                <p>03.2022 - </p>
+                                <p>
+                                    Frontend Developer • React web apps • UI development • Performance optimization •
+                                    Global
+                                    collaboration on UX
+                                </p>
+                            </div>
+                            <div>
+                                <h5>Spółdzielnia Mieszkaniowa Kopernik</h5>
+                                <p>2019 - 2019</p>
+                                <p>
+                                    Service Desk Consultant • IT support and troubleshooting • Assisting users with tech
+                                    issues • Keeping systems running smoothly
+                                </p>
+                            </div>
                         </div>
-                        <div>
-                            <p className="font-semibold">Spółdzielnia Mieszkaniowa Kopernik</p>
-                            <p>2019 - 2019</p>
-                            <p>
-                                Service Desk Consultant • IT support and troubleshooting • Assisting users with tech
-                                issues • Keeping systems running smoothly
-                            </p>
+                    </div>
+                    <div className="flex flex-col space-y-2">
+                        <h4>Education</h4>
+                        <div className="flex flex-col space-y-4 text-sm">
+                            <div>
+                                <h5>Warsaw University of Technology</h5>
+                                <p>2023 - </p>
+                                <p>Computer Science</p>
+                            </div>
+                            <div>
+                                <h5>Nicolaus Copernicus University</h5>
+                                <p>2019 - 2023</p>
+                                <p>Computer Science</p>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="flex-1 flex flex-col space-y-2">
-                    <h6>Education</h6>
+                    <h4>Contact and Socials</h4>
                     <div className="flex flex-col space-y-4 text-sm">
                         <div>
-                            <p className="font-semibold">Warsaw University of Technology</p>
-                            <p>2023 - </p>
-                            <p>Computer Science</p>
-                        </div>
-                        <div>
-                            <p className="font-semibold">Nicolaus Copernicus University</p>
-                            <p>2019 - 2023</p>
-                            <p>Computer Science</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex-1 flex flex-col space-y-2">
-                    <h6>Contact and Socials</h6>
-                    <div className="flex flex-col space-y-4 text-sm">
-                        <div>
-                            <p className="font-semibold">Email</p>
+                            <h5>Email</h5>
                             <p>fadinachawati17@gmail.com</p>
                         </div>
                         <div>
-                            <p className="font-semibold">Social</p>
-                            <p>linkedin</p>
-                            <p>github</p>
+                            <h5>Social</h5>
+                            <div className="flex space-x-2">
+                                <FaLinkedinIn className="text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl"/>
+                                <FaGithub className="text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl"/>
+                            </div>
                         </div>
                     </div>
                 </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className="navbar bg-transparent">
+        <nav className="navbar">
             <div className="navbar-logo">
                 FADI NACHAWATI
             </div>
             <div className="navbar-links">
-                <div className="flex-1 flex justify-center space-x-10">
+                <div className="flex-1 flex justify-center space-x-3 lg:space-x-4 xl:space-x-5 2xl:space-x-6">
                     <a href="">HOME</a>
                     <a href="">ABOUT</a>
                     <a href="">CONTACT</a>
                 </div>
             </div>
             <div className="navbar-socials">
-                <div className="flex-1 flex justify-center space-x-10">
+                <div className="flex-1 flex justify-center space-x-3 lg:space-x-4 xl:space-x-5 2xl:space-x-6">
                     <a href="">LINKEDIN</a>
                     <a href="">GITHUB</a>
                 </div>
