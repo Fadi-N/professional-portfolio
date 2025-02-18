2f723ee072f1e0d9119baf37d0407949aba81532
Fadi <fadi@Fadis-MacBook-Pro.local>
2025-02-18 22:30:27 +0100

    change | fully working overlapping card

--- components/overlapping-cards.tsx
+++ components/overlapping-cards.tsx
@@ -8,95 +8,145 @@ import React from "react";
 export default function OverlappingCards() {
     const {scrollYProgress} = useScroll();
 
-    const card1Y = useTransform(scrollYProgress, [0.15, .95], [260, -160]);
-    const card2Y = useTransform(scrollYProgress, [0.3, .95], [280, -120]);
-    const card3Y = useTransform(scrollYProgress, [0.45, .95], [340, -80]);
-    const card4Y = useTransform(scrollYProgress, [0.6, .95], [380, -40]);
-    const card5Y = useTransform(scrollYProgress, [0.75, .95], [420, 0]);
+    const cards = [
+        {
+            cardY: useTransform(scrollYProgress, [0.15, .95], [260, -160]),
+            bgColor: "bg-[#ccb987]",
+            year: "2024",
+            shortcut: "Website | 3D Model",
+            title: "Bringing the iPhone to life in 3D",
+            subtitle: "An immersive, real-time 3D experience built with Three.js & React Three Fiber",
+            description1: {
+                title: "360° Exploration",
+                description: "Interact with a fully-rendered iPhone model, exploring every detail from any angle."
+            },
+            description2: {
+                title: "Smooth Animations & UI",
+                description1: "GSAP-powered animations ensure seamless transitions and a dynamic user experience"
+            },
+            stack: "JavaScript, React, Three.js, React Three Fiber, GSAP, Tailwind CSS",
+            src: "/sample.jpg"
+        },
+        {
+            cardY: useTransform(scrollYProgress, [0.3, .95], [280, -120]),
+            bgColor: "bg-[#79a978]",
+            year: "2024",
+            shortcut: "Website | 3D Model",
+            title: "Bringing the iPhone to life in 3D",
+            subtitle: "An immersive, real-time 3D experience built with Three.js & React Three Fiber",
+            description1: {
+                title: "360° Exploration",
+                description: "Interact with a fully-rendered iPhone model, exploring every detail from any angle."
+            },
+            description2: {
+                title: "Smooth Animations & UI",
+                description1: "GSAP-powered animations ensure seamless transitions and a dynamic user experience"
+            },
+            stack: "JavaScript, React, Three.js, React Three Fiber, GSAP, Tailwind CSS",
+            src: "/sample.jpg"
+        },
+        {
+            cardY: useTransform(scrollYProgress, [0.45, .95], [340, -80]),
+            bgColor: "bg-[#74b5a5]",
+            year: "2024",
+            shortcut: "Website | 3D Model",
+            title: "Bringing the iPhone to life in 3D",
+            subtitle: "An immersive, real-time 3D experience built with Three.js & React Three Fiber",
+            description1: {
+                title: "360° Exploration",
+                description: "Interact with a fully-rendered iPhone model, exploring every detail from any angle."
+            },
+            description2: {
+                title: "Smooth Animations & UI",
+                description1: "GSAP-powered animations ensure seamless transitions and a dynamic user experience"
+            },
+            stack: "JavaScript, React, Three.js, React Three Fiber, GSAP, Tailwind CSS",
+            src: "/sample.jpg"
+        },
+        {
+            cardY: useTransform(scrollYProgress, [0.6, .95], [380, -40]),
+            bgColor: "bg-[#6ba6ef]",
+            year: "2024",
+            shortcut: "Website | 3D Model",
+            title: "Bringing the iPhone to life in 3D",
+            subtitle: "An immersive, real-time 3D experience built with Three.js & React Three Fiber",
+            description1: {
+                title: "360° Exploration",
+                description: "Interact with a fully-rendered iPhone model, exploring every detail from any angle."
+            },
+            description2: {
+                title: "Smooth Animations & UI",
+                description1: "GSAP-powered animations ensure seamless transitions and a dynamic user experience"
+            },
+            stack: "JavaScript, React, Three.js, React Three Fiber, GSAP, Tailwind CSS",
+            src: "/sample.jpg"
+        },
+        {
+            cardY: useTransform(scrollYProgress, [0.75, .95], [420, 0]),
+            bgColor: "bg-[#9c9cf8]",
+            year: "2024",
+            shortcut: "Website | 3D Model",
+            title: "Bringing the iPhone to life in 3D",
+            subtitle: "An immersive, real-time 3D experience built with Three.js & React Three Fiber",
+            description1: {
+                title: "360° Exploration",
+                description: "Interact with a fully-rendered iPhone model, exploring every detail from any angle."
+            },
+            description2: {
+                title: "Smooth Animations & UI",
+                description: "GSAP-powered animations ensure seamless transitions and a dynamic user experience"
+            },
+            stack: "JavaScript, React, Three.js, React Three Fiber, GSAP, Tailwind CSS",
+            src: "/sample.jpg"
+        }
+    ]
 
     return (
         <div className="overlapping-cards">
-            <motion.div
-                style={{y: card1Y}}
-                className="sticky top-0 h-screen flex items-center justify-center z-30"
-            >
-                <div className="overlapping-card bg-[#c59172] text-white">
-                    <div className="overlapping-card-header">
-                        <p>2024</p>
-                        <p>Website | 3D Model</p>
+            {cards.map((card, index) => (
+                <motion.div
+                    key={index}
+                    style={{y: card.cardY}}
+                    className="sticky top-0 h-screen flex items-center justify-center z-30"
+                >
+                    <div className={`overlapping-card text-white ${card.bgColor}`}>
+                        <div className="overlapping-card-header">
+                            <p>{card.year}</p>
+                            <p>{card.shortcut}</p>
+                        </div>
+                        <div className="overlapping-card-body">
+                            <div className="flex flex-col space-y-2">
+                                <div className="flex items-center justify-between">
+                                    <h1>{card.title}</h1>
+                                    <FaArrowRight
+                                        className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl transform transition-transform duration-300 hover:-rotate-45"/>
+                                </div>
+                                <h3>{card.subtitle}</h3>
+                            </div>
+                            <div className="flex md:space-x-10 lg:space-x-12 xl:space-x-16 2xl:space-x-20">
+                                <div>
+                                    <h4>{card.description1.title}</h4>
+                                    <p>{card.description1.description}</p>
+                                </div>
+                                <div>
+                                    <h4>{card.description2.title}</h4>
+                                    <p>{card.description2.description}</p>
+                                </div>
+                            </div>
+                            <p>Tech Stack – {card.stack}</p>
+                            <div className="flex-1">
+                                <Image
+                                    src={card.src}
+                                    alt="react.png"
+                                    fill={true}
+                                    className="rounded lg:rounded-md xl:rounded-lg 2xl:rounded-xl !relative !h-[510px]"
+                                />
+                            </div>
+                        </div>
                     </div>
-                    <div className="overlapping-card-body">
-                        <div className="flex flex-col space-y-2">
-                            <div className="flex items-center justify-between">
-                                <h1>Bringing the iPhone to life in 3D</h1>
-                                <FaArrowRight
-                                    className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl transform transition-transform duration-300 hover:-rotate-45"/>
-                            </div>
-                            <h3>An immersive, real-time 3D experience built with Three.js & React Three Fiber</h3>
-                        </div>
-                        <div className="flex md:space-x-10 lg:space-x-12 xl:space-x-16 2xl:space-x-20">
-                            <div>
-                                <h4>360° Exploration</h4>
-                                <p>
-                                    Interact with a fully-rendered iPhone model, exploring every detail from any angle.
-                                </p>
-                            </div>
-                            <div>
-                                <h4>Smooth Animations & UI</h4>
-                                <p>
-                                    GSAP-powered animations ensure seamless transitions and a dynamic user experience.
-                                </p>
-                            </div>
-                        </div>
-                        <p>Tech Stack – JavaScript, React, Three.js, React Three Fiber, GSAP, Tailwind CSS</p>
-                        <div className="flex-1">
-                            <Image
-                                src="/sample.jpg"
-                                alt="react.png"
-                                fill={true}
-                                className="rounded lg:rounded-md xl:rounded-lg 2xl:rounded-xl !relative"
-                            />
-                        </div>
-                    </div>
-                </div>
-            </motion.div>
 
-            <motion.div
-                style={{y: card2Y}}
-                className="sticky top-0 h-screen flex items-center justify-center z-30"
-            >
-                <div className="w-full h-[800px] bg-[#ccb987] text-white flex items-center justify-center rounded-lg">
-                    Card 2
-                </div>
-            </motion.div>
-
-            <motion.div
-                style={{y: card3Y}}
-                className="sticky top-0 h-screen flex items-center justify-center z-30"
-            >
-                <div
-                    className="w-full h-[800px] bg-[#79a978] text-white flex items-center justify-center rounded-lg">
-                    Card 3
-                </div>
-            </motion.div>
-            <motion.div
-                style={{y: card4Y}}
-                className="sticky top-0 h-screen flex items-center justify-center z-30"
-            >
-                <div
-                    className="w-full h-[800px] bg-[#6ba6ef] text-white flex items-center justify-center rounded-lg">
-                    Card 4
-                </div>
-            </motion.div>
-            <motion.div
-                style={{y: card5Y}}
-                className="sticky top-0 h-screen flex items-center justify-center z-30"
-            >
-                <div
-                    className="w-full h-[800px] bg-[#9c9cf8] text-white flex items-center justify-center rounded-lg">
-                    Card5
-                </div>
-            </motion.div>
+                </motion.div>
+            ))}
         </div>
     );
 }
