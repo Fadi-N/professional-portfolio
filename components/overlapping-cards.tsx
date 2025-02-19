a8f8934b6a9b516cd3f3aeb831bdde627c742242
Fadi <fadi@Fadis-MacBook-Pro.local>
2025-02-19 22:45:10 +0100

    bugfix | fixed ovelapping card scrolling and spaces

--- components/overlapping-cards.tsx
+++ components/overlapping-cards.tsx
@@ -1,113 +1,129 @@
 "use client"; // jeśli używasz Next.js 13+ z app router
 
-import {motion, useScroll, useTransform} from "framer-motion";
-import {FaArrowRight} from "react-icons/fa6";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { FaArrowRight } from "react-icons/fa6";
 import Image from "next/image";
 import React from "react";
 
 export default function OverlappingCards() {
-    const {scrollYProgress} = useScroll();
+    const { scrollYProgress } = useScroll();
 
     const cards = [
         {
-            cardY: useTransform(scrollYProgress, [0.4, .95], [100, -160]),
+            cardY: useTransform(scrollYProgress, [0.4, 0.95], [100, -160]),
             bgColor: "bg-[#ccb987]",
             year: "2024",
             shortcut: "Website | 3D Model",
             title: "Bringing the iPhone to life in 3D",
-            subtitle: "An immersive, real-time 3D experience built with Three.js & React Three Fiber",
+            subtitle:
+                "An immersive, real-time 3D experience built with Three.js & React Three Fiber",
             description1: {
                 title: "360° Exploration",
-                description: "Interact with a fully-rendered iPhone model, exploring every detail from any angle."
+                description:
+                    "Interact with a fully-rendered iPhone model, exploring every detail from any angle.",
             },
             description2: {
                 title: "Smooth Animations & UI",
-                description: "GSAP-powered animations ensure seamless transitions and a dynamic user experience"
+                description:
+                    "GSAP-powered animations ensure seamless transitions and a dynamic user experience",
             },
             stack: "JavaScript, React, Three.js, React Three Fiber, GSAP, Tailwind CSS",
-            src: "/sample.jpg"
+            src: "/sample.jpg",
         },
         {
-            cardY: useTransform(scrollYProgress, [0.3, .95], [280, -120]),
+            cardY: useTransform(scrollYProgress, [0.3, 0.95], [280, -120]),
             bgColor: "bg-[#79a978]",
             year: "2023",
             shortcut: "Website | PIM",
             title: "Transforming Content Creation with PIM Editor",
-            subtitle: "A dynamic tool for building product pages, blogs, and fully customizable views",
+            subtitle:
+                "A dynamic tool for building product pages, blogs, and fully customizable views",
             description1: {
                 title: "Limitless Customization",
-                description: "Prebuilt components, sliders, and flexible content blocks allow for unique layouts."
+                description:
+                    "Prebuilt components, sliders, and flexible content blocks allow for unique layouts.",
             },
             description2: {
                 title: "JSON-Driven & Real-Time Rendering",
-                description: "Every structure is powered by JSON, seamlessly converting into fully-rendered HTML."
+                description:
+                    "Every structure is powered by JSON, seamlessly converting into fully-rendered HTML.",
             },
             stack: "JavaScript, React, Redux, Bootstrap 5, JSON",
-            src: "/sample.jpg"
+            src: "/sample.jpg",
         },
         {
-            cardY: useTransform(scrollYProgress, [0.45, .95], [340, -80]),
+            cardY: useTransform(scrollYProgress, [0.45, 0.95], [340, -80]),
             bgColor: "bg-[#74b5a5]",
             year: "2022",
             shortcut: "Website | RentMe",
             title: "A Seamless Property Rental Experience",
-            subtitle: "Effortless booking, secure authentication, and personalized listings in one platform.",
+            subtitle:
+                "Effortless booking, secure authentication, and personalized listings in one platform.",
             description1: {
                 title: "Smart Authentication & Filtering",
-                description: "NextAuth.js enables secure login and OAuth (Google & GitHub), while advanced filters and \"Favorites\" help users find and save top listings."
+                description:
+                    'NextAuth.js enables secure login and OAuth (Google & GitHub), while advanced filters and "Favorites" help users find and save top listings.',
             },
             description2: {
                 title: "Smooth Reservations & Performance",
-                description: "Easily book, cancel, and track trips. Built with Next.js and Prisma for seamless routing, authentication, and data management."
+                description:
+                    "Easily book, cancel, and track trips. Built with Next.js and Prisma for seamless routing, authentication, and data management.",
             },
             stack: "JavaScript, React, Next.js, Tailwind, Prisma",
-            src: "/sample.jpg"
+            src: "/sample.jpg",
         },
         {
-            cardY: useTransform(scrollYProgress, [0.6, .95], [380, -40]),
+            cardY: useTransform(scrollYProgress, [0.6, 0.95], [380, -40]),
             bgColor: "bg-[#6ba6ef]",
             year: "2025",
             shortcut: "Website | Your Waiter | Coming soon",
             title: "Bringing the iPhone to life in 3D",
-            subtitle: "An immersive, real-time 3D experience built with Three.js & React Three Fiber",
+            subtitle:
+                "An immersive, real-time 3D experience built with Three.js & React Three Fiber",
             description1: {
                 title: "360° Exploration",
-                description: "Interact with a fully-rendered iPhone model, exploring every detail from any angle."
+                description:
+                    "Interact with a fully-rendered iPhone model, exploring every detail from any angle.",
             },
             description2: {
                 title: "Smooth Animations & UI",
-                description: "GSAP-powered animations ensure seamless transitions and a dynamic user experience"
+                description:
+                    "GSAP-powered animations ensure seamless transitions and a dynamic user experience",
             },
             stack: "JavaScript, React, Three.js, React Three Fiber, GSAP, Tailwind CSS",
-            src: "/sample.jpg"
+            src: "/sample.jpg",
         },
         {
-            cardY: useTransform(scrollYProgress, [0.75, .95], [420, 0]),
+            cardY: useTransform(scrollYProgress, [0.75, 0.95], [420, 0]),
             bgColor: "bg-[#9c9cf8]",
             year: "2025",
             shortcut: "Website | AI Gen | Comming soon",
             title: "Bringing the iPhone to life in 3D",
-            subtitle: "An immersive, real-time 3D experience built with Three.js & React Three Fiber",
+            subtitle:
+                "An immersive, real-time 3D experience built with Three.js & React Three Fiber",
             description1: {
                 title: "360° Exploration",
-                description: "Interact with a fully-rendered iPhone model, exploring every detail from any angle."
+                description:
+                    "Interact with a fully-rendered iPhone model, exploring every detail from any angle.",
             },
             description2: {
                 title: "Smooth Animations & UI",
-                description: "GSAP-powered animations ensure seamless transitions and a dynamic user experience"
+                description:
+                    "GSAP-powered animations ensure seamless transitions and a dynamic user experience",
             },
             stack: "JavaScript, React, Three.js, React Three Fiber, GSAP, Tailwind CSS",
-            src: "/sample.jpg"
-        }
-    ]
+            src: "/sample.jpg",
+        },
+    ];
 
     return (
-        <div className="overlapping-cards">
+        <div className="overlapping-cards relative">
             {cards.map((card, index) => (
                 <motion.div
                     key={index}
-                    style={{y: card.cardY}}
-                    className="sticky top-0 h-screen flex items-center justify-center z-30"
+                    style={{ y: card.cardY }}
+                    // Możesz eksperymentować z wysokością – tutaj 70vh
+                    className="sticky top-0 min-h-[70vh] flex items-center justify-center z-30"
                 >
                     <div className={`overlapping-card text-white ${card.bgColor}`}>
                         <div className="overlapping-card-header">
@@ -118,8 +134,7 @@ export default function OverlappingCards() {
                             <div className="flex flex-col space-y-2">
                                 <div className="flex items-baseline md:items-center justify-between">
                                     <h1>{card.title}</h1>
-                                    <FaArrowRight
-                                        className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl transform transition-transform duration-300 hover:-rotate-45"/>
+                                    <FaArrowRight className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl transform transition-transform duration-300 hover:-rotate-45" />
                                 </div>
                                 <div className="hidden md:block">
                                     <h3>{card.subtitle}</h3>
@@ -146,7 +161,6 @@ export default function OverlappingCards() {
                             </div>
                         </div>
                     </div>
-
                 </motion.div>
             ))}
         </div>
