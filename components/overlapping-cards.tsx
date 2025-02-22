b040f2d28c2a2c7d382171712c5febcd3495eae7
Fadi <fadi@Fadis-MacBook-Pro.local>
2025-02-22 21:31:25 +0100

    bugfix | scroll on overlapping card adjusted for devices

--- components/overlapping-cards.tsx
+++ components/overlapping-cards.tsx
@@ -1,21 +1,44 @@
-"use client"; // jeśli używasz Next.js 13+ z app router
+"use client";
 
-import {motion, useMotionValueEvent, useScroll, useTransform} from "framer-motion";
+import {motion, useScroll, useTransform} from "framer-motion";
 import {FaArrowRight} from "react-icons/fa6";
 import Image from "next/image";
-import React from "react";
+import React, {useEffect, useState} from "react";
+
+function useWindowSize() {
+    const [windowSize, setWindowSize] = useState({width: 1080, height: 1080});
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+
+        window.addEventListener("resize", handleResize);
+        handleResize();
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return windowSize;
+}
 
 export default function OverlappingCards() {
     const {scrollYProgress} = useScroll();
+    const {height} = useWindowSize();
 
-    useMotionValueEvent(scrollYProgress, "change", (latest) => {
-        const percentage = latest * 100;
-        console.log(`Scroll: ${percentage.toFixed(2)}%`);
-    });
+    const scaleFactor = height / 1080;
+
+    /*useMotionValueEvent(scrollYProgress, "change", (latest) => {
+            const percentage = latest * 100;
+            console.log(`Scroll: ${percentage.toFixed(2)}%`);
+        }
+    );*/
 
     const cards = [
         {
-            cardY: useTransform(scrollYProgress, [0, .39, 1], [0, 0, -400]),
+            cardY: useTransform(scrollYProgress, [0, 0.39, 1], [0, 0, -400 * scaleFactor]),
             bgColor: "bg-[#ccb987]",
             year: "2024",
             shortcut: "Website | 3D Model",
@@ -23,17 +46,21 @@ export default function OverlappingCards() {
             subtitle: "An immersive, real-time 3D experience built with Three.js & React Three Fiber",
             description1: {
                 title: "360° Exploration",
-                description: "Interact with a fully-rendered iPhone model, exploring every detail from any angle."
+                description: "Interact with a fully-rendered iPhone model, exploring every detail from any angle.",
             },
             description2: {
                 title: "Smooth Animations & UI",
-                description: "GSAP-powered animations ensure seamless transitions and a dynamic user experience"
+                description: "GSAP-powered animations ensure seamless transitions and a dynamic user experience",
             },
             stack: "JavaScript, React, Three.js, React Three Fiber, GSAP, Tailwind CSS",
-            src: "/sample.jpg"
+            src: "/sample.jpg",
         },
         {
-            cardY: useTransform(scrollYProgress, [0.23, 0.36, 1], [-200, 90, -300]),
+            cardY: useTransform(
+                scrollYProgress,
+                [0.23, 0.39, 1],
+                [-200 * scaleFactor, 90 * scaleFactor, -300 * scaleFactor]
+            ),
             bgColor: "bg-[#79a978]",
             year: "2023",
             shortcut: "Website | PIM",
@@ -41,17 +68,21 @@ export default function OverlappingCards() {
             subtitle: "A dynamic tool for building product pages, blogs, and fully customizable views",
             description1: {
                 title: "Limitless Customization",
-                description: "Prebuilt components, sliders, and flexible content blocks allow for unique layouts."
+                description: "Prebuilt components, sliders, and flexible content blocks allow for unique layouts.",
             },
             description2: {
                 title: "JSON-Driven & Real-Time Rendering",
-                description: "Every structure is powered by JSON, seamlessly converting into fully-rendered HTML."
+                description: "Every structure is powered by JSON, seamlessly converting into fully-rendered HTML.",
             },
             stack: "JavaScript, React, Redux, Bootstrap 5, JSON",
-            src: "/sample.jpg"
+            src: "/sample.jpg",
         },
         {
-            cardY: useTransform(scrollYProgress, [0.36, 0.48, 1], [-200, 90, -200]),
+            cardY: useTransform(
+                scrollYProgress,
+                [0.39, 0.52, 1],
+                [-200 * scaleFactor, 90 * scaleFactor, -200 * scaleFactor]
+            ),
             bgColor: "bg-[#74b5a5]",
             year: "2022",
             shortcut: "Website | RentMe",
@@ -59,17 +90,23 @@ export default function OverlappingCards() {
             subtitle: "Effortless booking, secure authentication, and personalized listings in one platform.",
             description1: {
                 title: "Smart Authentication & Filtering",
-                description: "NextAuth.js enables secure login and OAuth (Google & GitHub), while advanced filters and \"Favorites\" help users find and save top listings."
+                description:
+                    "NextAuth.js enables secure login and OAuth (Google & GitHub), while advanced filters and \"Favorites\" help users find and save top listings.",
             },
             description2: {
                 title: "Smooth Reservations & Performance",
-                description: "Easily book, cancel, and track trips. Built with Next.js and Prisma for seamless routing, authentication, and data management."
+                description:
+                    "Easily book, cancel, and track trips. Built with Next.js and Prisma for seamless routing, authentication, and data management.",
             },
             stack: "TypeScript, React, Next.js, Tailwind, Prisma",
-            src: "/sample.jpg"
+            src: "/sample.jpg",
         },
         {
-            cardY: useTransform(scrollYProgress, [0.48, 0.61, 1], [-200, 90, -100]),
+            cardY: useTransform(
+                scrollYProgress,
+                [0.52, 0.65, 1],
+                [-200 * scaleFactor, 90 * scaleFactor, -100 * scaleFactor]
+            ),
             bgColor: "bg-[#6ba6ef]",
             year: "2025",
             shortcut: "Website | Your Waiter | Coming soon",
@@ -77,34 +114,42 @@ export default function OverlappingCards() {
             subtitle: "QR code ordering, instant payments, and a seamless restaurant experience.",
             description1: {
                 title: "Order, Pay & Enjoy – All from Your Phone",
-                description: "Simply scan a QR code to explore the menu, place orders, and pay—no need to wait for a waiter"
+                description:
+                    "Simply scan a QR code to explore the menu, place orders, and pay—no need to wait for a waiter",
             },
             description2: {
                 title: "Optimized Workflow & Faster Service",
-                description: "Automated order processing, reduced wait times, and better table management for an improved dining experience."
+                description:
+                    "Automated order processing, reduced wait times, and better table management for an improved dining experience.",
             },
             stack: "TypeScript, React, Next.js, Tailwind CSS, MongoDB, Stripe",
-            src: "/sample.jpg"
+            src: "/sample.jpg",
         },
         {
-            cardY: useTransform(scrollYProgress, [0.61, 0.73, 1], [-200, 100, 0]),
+            cardY: useTransform(
+                scrollYProgress,
+                [0.65, 0.78, 1],
+                [-200 * scaleFactor, 90 * scaleFactor, 0 * scaleFactor]
+            ),
             bgColor: "bg-[#9c9cf8]",
             year: "2025",
-            shortcut: "Website | AIGen | Comming soon",
+            shortcut: "Website | AIGen | Coming soon",
             title: "AI-Powered Code Documentation & Quiz Generator",
             subtitle: "Transforming code into clear documentation and interactive learning experiences.",
             description1: {
                 title: "Automated Documentation Generation",
-                description: "Generate structured, easy-to-read technical documentation directly from your code, enhancing clarity and collaboration."
+                description:
+                    "Generate structured, easy-to-read technical documentation directly from your code, enhancing clarity and collaboration.",
             },
             description2: {
                 title: "Interactive Quizzes & Learning",
-                description: "Convert documentation into quizzes to reinforce knowledge, test understanding, and improve developer engagement"
+                description:
+                    "Convert documentation into quizzes to reinforce knowledge, test understanding, and improve developer engagement",
             },
             stack: "React, FastAPI, Tailwind CSS, OpenAI API",
-            src: "/sample.jpg"
-        }
-    ]
+            src: "/sample.jpg",
+        },
+    ];
 
     return (
         <div className="overlapping-cards">
