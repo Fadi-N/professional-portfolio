296f8d3d0d0edbf00f0858a9f68fa60708a6d33d
Fadi <fadi@Fadis-MacBook-Pro.local>
2025-02-23 17:14:41 +0100

    bugfix | deploy bugfix

--- components/overlapping-cards.tsx
+++ components/overlapping-cards.tsx
@@ -2,7 +2,6 @@
 
 import {motion, useMotionValueEvent, useScroll, useTransform} from "framer-motion";
 import {FaArrowRight} from "react-icons/fa6";
-import Image from "next/image";
 import React, {useEffect, useState} from "react";
 
 function useWindowSize() {
